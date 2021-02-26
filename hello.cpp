8be7fbef671ef99a84047694d501f5b9bac25d2e
brforry <67486262+brforry@users.noreply.github.com>
2021-02-25 17:15:18 -0700

    testing

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+// hello.cpp file
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello is it me your looking ?" << endl;
+    // std::cout << "Hello is it me your looking ?" << std::endl;
+    return 0;
+}
